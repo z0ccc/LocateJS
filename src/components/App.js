0b7290856bba80351f8e340dc4102101111732d4
z0ccc <z0cccccccc@gmail.com>
2021-09-15 12:45:23 -0400

    Initialize

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import '../App.css';
+import MainColumn from './MainColumn';
+
+const App = () => (
+  <div className="App">
+    <MainColumn />
+  </div>
+);
+
+export default App;
---
+++ src/components/MainColumn.js
@@ -0,0 +1,13 @@
+// import Logo from './Logo';
+// import StartBlock from './StartBlock';
+// import ScanBlocks from './ScanBlocks';
+
+const MainColumn = () => (
+  <div className="centerBlockOuter">
+    <div className="centerBlockInner">
+      hello
+      {/* <Logo /> */}
+    </div>
+  </div>
+);
+export default MainColumn;
