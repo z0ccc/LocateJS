42cbe78e5ce1b252066ecbaa5647ffd4cbbcae5e
z0ccc <z0cccccccc@gmail.com>
2021-09-20 21:12:51 -0400

    Added StartBlock with scan button/ added meta images

--- src/components/MainColumn.js
+++ src/components/MainColumn.js
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import Logo from './Logo';
 import Blocks from './Blocks';
+import StartBlock from './StartBlock';
 
-const MainColumn = () => (
-  <div className="centerBlockOuter">
-    <div className="centerBlockInner">
-      <Logo />
-      <Blocks />
+const MainColumn = () => {
+  const [scan, setScan] = useState(false);
+
+  return (
+    <div className="centerBlockOuter">
+      <div className="centerBlockInner">
+        <Logo />
+        {scan ? <Blocks /> : <StartBlock scan={scan} setScan={setScan} />}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default MainColumn;
