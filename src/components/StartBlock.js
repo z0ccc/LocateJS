d0af6ce237155bf41d25c6b44126de7c0b1a7bbf
z0ccc <z0cccccccc@gmail.com>
2021-09-21 01:44:47 -0400

    Added startblock body text

--- src/components/StartBlock.js
+++ src/components/StartBlock.js
@@ -6,15 +6,15 @@ const listItems = [
   {
     title: 'Predict Location',
     icon: 'predict',
-    body: 'Browsers reveal bits of identifiable information. This data can be combined into a digital fingerprint which can be used to follow you around the web.',
+    body: 'Connection and system data can be examined to determine if the info is accurate. Accurate info can then be further evaluated to determine location.',
   },
   {
     title: 'Connection',
     icon: 'connection',
-    body: 'Websites are able to access your IP address when you connect to their server. Your IP address exposes information about your connection and location.',
+    body: 'Every device connecting to the internet is assigned an ip address. Information about your location can be exposed by analyzing your ip address.',
   },
   {
-    title: 'System Info',
+    title: 'System Data',
     icon: 'system',
     body: "JavaScript can be used to find data about your computer's software and hardware. This information can be used to create a fingerprint.",
   },
@@ -24,9 +24,9 @@ const StartBlock = ({ setScan }) => (
   <Block>
     <h2>About</h2>
     <div className="contentBody">
-      Vytal shows you what traces your browser leaves behind while surfing the
-      web. This scan allows you to understand how easy it is to identify and
-      track your browser even while using private mode.
+      LocateJS predicts your location by analysing your connection and system
+      data. This scan allows you to understand how your location can be
+      pinpointed even while using a vpn, location spoofer or private mode.
     </div>
     <StartList items={listItems} />
     <input
