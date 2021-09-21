79fb2bb8e295519ca4521273f2828d81ce394e11
z0ccc <z0cccccccc@gmail.com>
2021-09-21 19:12:16 -0400

    Fixed logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,40 +1,9 @@
+import { ReactComponent as LogoImg } from '../images/logo.svg';
+
 const Logo = () => (
   <div className="logoWrap">
     <a href="." className="logo" alt="Vytal logo">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        x="0"
-        y="0"
-        version="1.1"
-        viewBox="0 0 2667.967 511.999"
-        xmlSpace="preserve"
-      >
-        <g fill="#fff" transform="translate(-70.573)">
-          <g fill="#fff">
-            <path
-              fill="#fff"
-              d="M256 0C153.755 0 70.573 83.182 70.573 185.426c0 126.888 165.939 313.167 173.004 321.035 6.636 7.391 18.222 7.378 24.846 0 7.065-7.868 173.004-194.147 173.004-321.035C441.425 83.182 358.244 0 256 0zm0 278.719c-51.442 0-93.292-41.851-93.292-93.293 0-51.442 41.851-93.292 93.292-93.292s93.291 41.851 93.291 93.293c0 51.442-41.85 93.292-93.291 93.292z"
-            />
-          </g>
-        </g>
-        <text
-          xmlSpace="preserve"
-          style={{ lineHeight: '1.25' }}
-          x="471.832"
-          y="453.916"
-          fill="#fff"
-          fontFamily="Comfortaa"
-          fontSize="506.667"
-          fontStretch="normal"
-          fontStyle="normal"
-          fontVariant="normal"
-          fontWeight="bold"
-        >
-          <tspan x="471.832" y="453.916">
-            LocateJS
-          </tspan>
-        </text>
-      </svg>
+      <LogoImg />
     </a>
   </div>
 );
