1c74f624a4b94777136c285f5d5f856b87fa1f31
z0ccc <z0cccccccc@gmail.com>
2021-12-20 21:27:09 -0500

    Removed start block

--- src/components/MainColumn.js
+++ src/components/MainColumn.js
@@ -1,20 +1,14 @@
 import './MainColumn.css';
-import { useState } from 'react';
 import Logo from './Logo';
 import Blocks from './Blocks';
-import StartBlock from './StartBlock';
 
-const MainColumn = () => {
-  const [scan, setScan] = useState(false);
-
-  return (
-    <div className="centerBlockOuter">
-      <div className="centerBlockInner">
-        <Logo />
-        {scan ? <Blocks /> : <StartBlock scan={scan} setScan={setScan} />}
-      </div>
+const MainColumn = () => (
+  <div className="centerBlockOuter">
+    <div className="centerBlockInner">
+      <Logo />
+      <Blocks />
     </div>
-  );
-};
+  </div>
+);
 
 export default MainColumn;
