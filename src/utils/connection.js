032a8fac89aa36e2ff8800e9f84bfb6d5da65db6
z0ccc <z0cccccccc@gmail.com>
2021-12-28 03:02:38 -0500

    Cleaned up connection block

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -10,76 +10,65 @@ const fetchAPI = (setData) => {
 };
 
 // Returns object with connection data
-const getConnection = (connectionData, workerData) => {
-  const timeZoneIssue = compareTimeZone(
-    connectionData.timezone,
-    workerData.timeZone
-  );
+const getConnection = (connectionData) => {
   const isProxy = checkProxy(connectionData.proxy);
   return [
     {
       key: 'IP address',
       value: connectionData.query,
-      issues: [timeZoneIssue, isProxy],
+      issues: [isProxy],
     },
     {
       key: 'Country',
       value: connectionData.country,
-      issues: [timeZoneIssue, isProxy],
+      issues: [isProxy],
     },
     {
       key: 'Region',
       value: connectionData.regionName,
-      issues: [timeZoneIssue, isProxy],
+      issues: [isProxy],
     },
     {
       key: 'City',
       value: connectionData.city,
-      issues: [timeZoneIssue, isProxy],
+      issues: [isProxy],
     },
     {
       key: 'Time zone',
       value: connectionData.timezone,
-      issues: [timeZoneIssue, isProxy],
+      issues: [isProxy],
     },
     {
       key: 'Zip code',
       value: connectionData.zip,
-      issues: [timeZoneIssue, isProxy],
+      issues: [isProxy],
     },
     {
       key: 'Latitude',
       value: connectionData.lat,
-      issues: [timeZoneIssue, isProxy],
+      issues: [isProxy],
     },
     {
       key: 'Longitude',
       value: connectionData.lon,
-      issues: [timeZoneIssue, isProxy],
+      issues: [isProxy],
     },
     {
       key: 'ISP',
       value: connectionData.isp,
-      issues: [timeZoneIssue, isProxy],
+      issues: [isProxy],
     },
     {
       key: 'Org',
       value: connectionData.org,
-      issues: [timeZoneIssue, isProxy],
+      issues: [isProxy],
     },
     {
       key: 'ASN',
       value: connectionData.as,
-      issues: [timeZoneIssue, isProxy],
+      issues: [isProxy],
     },
   ];
-};
-
-const compareTimeZone = (connectionTimeZone, workerTimeZone) => {
-  if (connectionTimeZone !== workerTimeZone) {
-    return "Connection data doesn't match system data";
-  }
-  return null;
 };
 
 const checkProxy = (proxy) => {
