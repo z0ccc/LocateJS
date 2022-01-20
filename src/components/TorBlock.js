6e113f7b41714cbde1b08c505a55a78556c00575
z0ccc <z0cccccccc@gmail.com>
2022-01-19 22:12:47 -0500

    Fixed tor detection on chrome

--- src/components/TorBlock.js
+++ src/components/TorBlock.js
@@ -1,25 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Block from './Block';
 import Table from './Table';
 
 const detectTor = (setIsTor) => {
-  const css = document.createElement('link');
-  css.href = 'resource://torbutton-assets/aboutTor.css';
-  css.type = 'text/css';
-  css.rel = 'stylesheet';
-  document.head.appendChild(css);
-  css.onload = () => {
-    setIsTor('True');
-  };
-  css.onerror = () => {
+  if (window.chrome) {
     setIsTor('False');
-  };
-  document.head.removeChild(css);
+  } else {
+    const css = document.createElement('link');
+    css.href = 'resource://torbutton-assets/aboutTor.css';
+    css.type = 'text/css';
+    css.rel = 'stylesheet';
+    document.head.appendChild(css);
+    css.onload = () => {
+      setIsTor('True');
+    };
+    css.onerror = () => {
+      setIsTor('False');
+    };
+    document.head.removeChild(css);
+  }
 };
 
 const TorBlock = () => {
   const [isTor, setIsTor] = useState();
-  detectTor(setIsTor);
+  useEffect(() => {
+    detectTor(setIsTor);
+  }, []);
   return (
     <Block>
       <h1>Tor Browser</h1>
