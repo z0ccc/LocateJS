1d52ecf264562c2037e139bc39a877c51a39b890
z0ccc <z0ccc@protonmail.com>
2022-02-08 23:02:43 -0500

    Switched to new tablerow component to connectionblock

--- src/components/ConnectionBlock.js
+++ src/components/ConnectionBlock.js
@@ -1,15 +1,24 @@
 import { useContext } from 'react';
 import DataContext from './Context';
 import Block from './Block';
-import Table from './Table';
 import { getConnection } from '../utils/connection';
+import TableRow from './TableRow2';
 
 const ConnectionBlock = () => {
   const { workerData, connectionData } = useContext(DataContext);
+
   return (
     <Block>
       <h1>Connection</h1>
-      <Table data={getConnection(connectionData, workerData)} />
+      <div className="tableWrap">
+        <table>
+          <tbody>
+            {getConnection(connectionData, workerData).map((item) => (
+              <TableRow key={item.key} title={item.key} value={item.value} issues={item.issues} />
+            ))}
+          </tbody>
+        </table>
+      </div>
       <p>
         <b>Explanation:</b> Your IP address reveals information about your
         connection. Using a VPN or Tor will hide your connection info.
