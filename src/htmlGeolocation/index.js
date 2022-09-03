a7c05bb9036bb3973447453598ab65f8e6960efc
z0ccc <z0ccc@proton.me>
2022-09-03 16:52:45 -0400

    html geolocation api

---
+++ src/htmlGeolocation/index.js
@@ -0,0 +1,34 @@
+const getGeocode = (lat, long) =>
+  fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=AIzaSyByyRWGncal9iAq1-3Ek2WQ3ROLw9bCS-8`
+  )
+    .then((response) => response.json())
+    .then((data) => data.results[0].formatted_address);
+
+const getHtmlGeolocation = async () =>
+  new Promise((showPosition, showError) => {
+    navigator.geolocation.getCurrentPosition(showPosition, showError, {
+      enableHighAccuracy: true,
+    });
+  })
+    .then(async (position) => {
+      const geocode = await getGeocode(
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      const htmlGeolocation = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        accuracy: position.coords.accuracy,
+        altitude: position.coords.altitude,
+        altitudeAccuracy: position.coords.altitudeAccuracy,
+        heading: position.coords.heading,
+        speed: position.coords.speed,
+        geocoding: geocode,
+      };
+
+      return htmlGeolocation;
+    })
+    .catch((e) => e.message);
+
+export default getHtmlGeolocation;
--- src/main.js
+++ src/main.js
@@ -2,31 +2,36 @@ import systemData from './systemData';
 import getWorkerData from './workerData';
 import detectTor from './detectTor';
 import getWebRTCData from './webRtc';
+import getHtmlGeolocation from './htmlGeolocation';
 
-Promise.all([getWorkerData(), getWebRTCData()]).then((promiseValues) => {
-  const workerData = promiseValues[0];
-  const webRtc = promiseValues[1];
-  let data = {};
-  Object.entries(systemData).forEach(([key]) => {
+Promise.all([getWorkerData(), getWebRTCData(), getHtmlGeolocation()]).then(
+  (promiseValues) => {
+    const workerData = promiseValues[0];
+    const webRtc = promiseValues[1];
+    const htmlGeolocation = promiseValues[2];
+    let data = {};
+    Object.entries(systemData).forEach(([key]) => {
+      data = {
+        ...data,
+        [key]: {
+          topWindow: {
+            value: systemData[key].value,
+            tampered: systemData[key].tampered,
+          },
+          webWorker: {
+            value: workerData[key].value,
+            tampered: workerData[key].tampered,
+          },
+        },
+      };
+    });
+
     data = {
       ...data,
-      [key]: {
-        topWindow: {
-          value: systemData[key].value,
-          tampered: systemData[key].tampered,
-        },
-        webWorker: {
-          value: workerData[key].value,
-          tampered: workerData[key].tampered,
-        },
-      },
+      webRtc,
+      tor: detectTor(),
+      htmlGeolocation,
     };
-  });
-
-  data = {
-    ...data,
-    webRtc,
-    tor: detectTor(),
-  };
-  console.log(data);
-});
+    console.log(data);
+  }
+);
