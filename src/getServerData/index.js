527d50ce955712cc2898c188bb4ac3b82d179c71
z0ccc <z0ccc@proton.me>
2022-09-19 01:26:27 -0400

    get a return server data

---
+++ src/getServerData/index.js
@@ -0,0 +1,4 @@
+const getServerData = () =>
+  fetch('http://localhost:8000/api/ip').then((response) => response.json());
+
+export default getServerData;
--- src/main.js
+++ src/main.js
@@ -1,37 +1,43 @@
 import systemData from './systemData';
+import getServerData from './getServerData';
 import getWorkerData from './workerData';
 import detectTor from './detectTor';
 import getWebRTCData from './webRtc';
 import getHtmlGeolocation from './htmlGeolocation';
 
-Promise.all([getWorkerData(), getWebRTCData(), getHtmlGeolocation()]).then(
-  (promiseValues) => {
-    const workerData = promiseValues[0];
-    const webRtc = promiseValues[1];
-    const htmlGeolocation = promiseValues[2];
-    let data = {};
-    Object.entries(systemData).forEach(([key]) => {
-      data = {
-        ...data,
-        [key]: {
-          topWindow: {
-            value: systemData[key].value,
-            tampered: systemData[key].tampered,
-          },
-          webWorker: {
-            value: workerData[key].value,
-            tampered: workerData[key].tampered,
-          },
+Promise.all([
+  getServerData(),
+  getWorkerData(),
+  getWebRTCData(),
+  getHtmlGeolocation(),
+]).then((promiseValues) => {
+  const serverData = promiseValues[0];
+  const workerData = promiseValues[1];
+  const webRtc = promiseValues[2];
+  const htmlGeolocation = promiseValues[3];
+  let clientData = {};
+  Object.entries(systemData).forEach(([key]) => {
+    clientData = {
+      ...clientData,
+      [key]: {
+        topWindow: {
+          value: systemData[key].value,
+          tampered: systemData[key].tampered,
         },
-      };
-    });
-
-    data = {
-      systemData: { ...data },
-      webRtc,
-      tor: detectTor(),
-      htmlGeolocation,
+        webWorker: {
+          value: workerData[key].value,
+          tampered: workerData[key].tampered,
+        },
+      },
     };
-    console.log(data);
-  }
-);
+  });
+
+  clientData = {
+    serverData,
+    systemData: { ...clientData },
+    webRtc,
+    tor: detectTor(),
+    htmlGeolocation,
+  };
+  console.log(clientData);
+});
